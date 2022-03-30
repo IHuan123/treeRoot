7abbf49aa8e4842f21dda7fe5f6dbf1be1edefa5
wanghuan <super_huan@outlook.com>
2022-03-30 11:07:44 +0800

    up

--- src/utils/canvas.ts
+++ src/utils/canvas.ts
@@ -2,7 +2,7 @@ export interface CanvasContext {
     ctx: CanvasRenderingContext2D 
     dpi: number
 }
-export function initCanvas(width = 400, height = 400, _dpi?: number):CanvasContext {
+export function initCanvas(width = 400, height = 400, bgColor: string = "#333", _dpi?: number):CanvasContext {
     let canvas: HTMLCanvasElement
     canvas = document.getElementById("canvas") as HTMLCanvasElement;
     // 如何dom节点中没有canvas则新建一个
@@ -11,6 +11,7 @@ export function initCanvas(width = 400, height = 400, _dpi?: number):CanvasConte
         canvas.id = "canvas"
         document.body.appendChild(canvas)
     }
+    canvas.style.background = bgColor
     const ctx:CanvasRenderingContext2D  = canvas.getContext('2d')!;
     const dpr = window.devicePixelRatio || 1; //像素比
 
