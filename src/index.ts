f84777f54faf8c6a20aaf1b64cd86f3f23d6f9af
wanghuan <1361035584@qq.com>
2022-03-30 17:53:15 +0800

    执行生命周期调整

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Plum from "@/utils/plum"
 import "../static/style/index.scss"
 // const bgImage = require("../static/images/demo.png")
-(function () {
+
+window.onload = function(){
   new Plum({watchWindowSize:true})
-})()
+}
